b4362bc5166b0088ed847cd1428eacba0774fc7b
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2024-02-01 19:01:51 +0100

    Initialize the flake

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  description = "A nixpkgs analogue for Kadena's packages";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = {...}@inputs: {
+
+  };
+}
