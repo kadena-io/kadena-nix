ea006870c6d5a13c6d63ff5855a81aa0219aefa6
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2024-05-14 17:23:08 +0200

    Move nixpkgs pin back to 23.11
    Primarilty so that we're more compatible with the devnet repo

--- flake.nix
+++ flake.nix
@@ -5,22 +5,23 @@
     # we can avoid this input when this issue is fixed:
     # https://github.com/NixOS/nixpkgs/issues/294183
     nixpkgs-23-05.url = "github:NixOS/nixpkgs/23.05";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
     let inherit (inputs) self;
         pkgs = inputs.nixpkgs.legacyPackages.${system};
         pkgs-23-05 = inputs.nixpkgs-23-05.legacyPackages.${system};
+        nodejs-slim = pkgs.nodejs-slim_20;
         nodePackages = import "${self}/pkgs/nodePackages" {
           inherit pkgs system;
-          nodejs = pkgs.nodejs;
+          nodejs = pkgs.nodejs_20;
         };
         graph = import pkgs/graph {
-          inherit pkgs system nodePackages;
+          inherit pkgs system nodePackages nodejs-slim;
         };
         kadena-cli = import pkgs/kadena-cli {
-          inherit pkgs system nodePackages;
+          inherit pkgs system nodePackages nodejs-slim;
           rollup = pkgs-23-05.nodePackages.rollup;
         };
     in {
